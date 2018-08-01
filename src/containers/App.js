0f16f3a155290f5c72f09850ab2958aa36390f7d
malik.l <malik.l@tc-gaming.com>
2018-08-01 19:18:34 +0800

    videos

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,65 @@
 import React, { Component } from "react";
+// ANTD.
+import { Layout, Icon, Button, Checkbox } from "antd";
+// Apollo.
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+// Stylesheet.
 import "../assets/stylesheets/App.css";
+
+const VIDEOS_PER_PAGE = 20;
+const VIDEOS_QUERY = gql`
+  query VideoQuery($filter: String, $first: Int, $skip: Int, $orderBy: VideoOrderByInput) {
+    videos(filter: $filter, first: $first, skip: $skip, orderBy: $orderBy) {
+      id,
+      name,
+      description,
+      category {
+        id
+        name
+      }
+      owner {
+        id
+        name
+        email
+      }
+      createdAt
+      }
+  }
+`;
+
 class App extends Component {
   constructor() {
     super();
     this.state = {};
   }
 
+  _getQueryVariables = () => {
+    const page = parseInt(this.props.match.params.page, 10);
+
+    const skip = 0;
+    const first = VIDEOS_PER_PAGE;
+    const orderBy = "createdAt_DESC";
+    return { first, skip, orderBy };
+  };
+
   render() {
+    const authToken = localStorage.getItem("AUTH_TOKEN");
+
     return (
-      <div className="App container-root">
-        首页
-      </div>
+      <Query
+        query={VIDEOS_QUERY}
+        variables={this._getQueryVariables()}
+        pollInterval={5000}
+        notifyOnNetworkStatusChange
+      >
+        {({ loading, error, data, refetch, networkStatus }) => {
+          if (networkStatus === 4) return "Refetching!";
+          if (loading) return <div>Fetching</div>;
+          if (error) return `Error!: ${error}`;
+          return <Layout className="App container-dashboard" />;
+        }}
+      </Query>
     );
   }
 }
